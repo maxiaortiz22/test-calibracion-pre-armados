0f4a2c12b0475f4da9e0d53038b2063409fbe81a
Maximiliano Ortiz <maxiaortiz22@gmail.com>
2023-03-10 17:00:13 -0300

    Cambio de interfaz

--- main.py
+++ main.py
@@ -201,57 +201,76 @@ if __name__ == '__main__':
     customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
     root = customtkinter.CTk()
-    root.title("Informe de pruebas de audio")
-    root.geometry("330x580")
+    root.title("Test de calibración de pre-armados")
+    root.geometry("670x580")
     root.iconbitmap('logo.ico')
 
     recomendacion0 = customtkinter.CTkLabel(root, text='Seleccione el tipo de auricular:')
-    recomendacion0.grid(row=0, column=0, pady=5, padx=50)
+    recomendacion0.grid(row=0, column=1, pady=5, padx=10)
     tipo_auricular = customtkinter.CTkOptionMenu(root, values=["Supraural (ej: JBL600)", "Circumaural (ej: JBL750)", "Vincha osea"])
-    tipo_auricular.grid(row=2, column=0, pady=5, padx=50)
+    tipo_auricular.grid(row=2, column=1, pady=5, padx=10)
     tipo_auricular.set("Supraural (ej: JBL600)")
 
     recomendacion1 = customtkinter.CTkLabel(root, text='Calibrar cada vez que mueva la ganancia')
-    recomendacion1.grid(row=4, column=0, pady=5, padx=50)
+    recomendacion1.grid(row=4, column=0, pady=5, padx=10)
 
     cal_low = customtkinter.CTkButton(root, text="Calibración", command=calibracion)
-    cal_low.grid(row=5, column=0, pady=5, padx=50)
+    cal_low.grid(row=5, column=0, pady=5, padx=10)
+
+    recomendacion_channel = customtkinter.CTkLabel(root, text='Elija el canal')
+    recomendacion_channel.grid(row=4, column=2, pady=5, padx=10)
+
+    channel = customtkinter.CTkOptionMenu(root, values=["Izquierdo", "Derecho"])
+    channel.grid(row=5, column=2, pady=5, padx=10)
 
     recomendacion1 = customtkinter.CTkLabel(root, text='Pruebas:')
-    recomendacion1.grid(row=6, column=0, pady=5, padx=50)
+    recomendacion1.grid(row=6, column=0, pady=5, padx=10)
 
-    record_linelidad_osea = customtkinter.CTkButton(root, text="Linealidad ósea", command=linealidad_osea)
-    record_linelidad_osea.grid(row=7, column=0, pady=5, padx=50)
+    recomendacion1_test = customtkinter.CTkLabel(root, text='Ajuste de medición:')
+    recomendacion1_test.grid(row=6, column=2, pady=5, padx=10)
 
-    record_linelidad_aerea = customtkinter.CTkButton(root, text="Linealidad aérea", command=linealidad_aerea)
-    record_linelidad_aerea.grid(row=8, column=0, pady=5, padx=50)
+    #De este mismo voy a sacar el Frequency Accuracy Test:
+    hearing_level = customtkinter.CTkButton(root, text="Nivel auditivo", command=linealidad_osea)
+    hearing_level.grid(row=7, column=0, pady=5, padx=10)
 
-    record_on_off = customtkinter.CTkButton(root, text="On/Off", command=on_off_time)
-    record_on_off.grid(row=9, column=0, pady=5, padx=50)
+    hearing_level_frec = customtkinter.CTkOptionMenu(root, values=["125", "250", "500", "750", "1000", "1500",
+                                                                 "2000", "3000", "4000", "6000", "8000"])
+    hearing_level_frec.grid(row=7, column=2, pady=5, padx=10)
+
+    narrow_band = customtkinter.CTkButton(root, text="Nivel de banda estrecha", command=linealidad_aerea)
+    narrow_band.grid(row=8, column=0, pady=5, padx=10)
+
+    narrow_band_frec = customtkinter.CTkOptionMenu(root, values=["125", "250", "500", "750", "1000", "1500",
+                                                                 "2000", "3000", "4000", "6000", "8000"])
+    narrow_band_frec.grid(row=8, column=2, pady=5, padx=10)
+
+    linearity = customtkinter.CTkButton(root, text="Linealidad", command=on_off_time)
+    linearity.grid(row=9, column=0, pady=5, padx=10)
+
+    linearity_level = customtkinter.CTkOptionMenu(root, values=["65 dBHL", "60 dBHL", "50 dBHL", "45 dBHL"])
+    linearity_level.grid(row=8, column=2, pady=5, padx=10)
 
     record_tono_pulsante = customtkinter.CTkButton(root, text="Tono pulsante", command=tono_pulsante)
-    record_tono_pulsante.grid(row=10, column=0, pady=5, padx=50)
+    record_tono_pulsante.grid(row=10, column=0, pady=5, padx=10)
 
     record_warble_tone = customtkinter.CTkButton(root, text="Warble tone", command=warble_tone)
-    record_warble_tone.grid(row=11, column=0, pady=5, padx=50)
+    record_warble_tone.grid(row=11, column=0, pady=5, padx=10)
 
-    record_nivel_vocal = customtkinter.CTkButton(root, text="Nivel vocal", command=nivel_vocal)
-    record_nivel_vocal.grid(row=12, column=0, pady=5, padx=50)
-
-    record_ruido = customtkinter.CTkButton(root, text="Ruido", command=ruido)
-    record_ruido.grid(row=13, column=0, pady=5, padx=50)
+    warble_tone_frec = customtkinter.CTkOptionMenu(root, values=["125", "250", "500", "750", "1000", "1500",
+                                                                 "2000", "3000", "4000", "6000", "8000"])
+    warble_tone_frec.grid(row=11, column=2, pady=5, padx=10)
 
     global progress_label
     progress_label = StringVar()
     progress_label.set("")
     recomendacion2 = customtkinter.CTkLabel(root, textvariable=progress_label)
-    recomendacion2.grid(row=14, column=0, pady=5, padx=50)
+    recomendacion2.grid(row=13, column=1, pady=5, padx=10)
 
     progress = customtkinter.CTkProgressBar(root)
-    progress.grid(row=15, column=0, pady=5, padx=50)
+    progress.grid(row=14, column=1, pady=5, padx=10)
     progress.set(0)
 
     calculate = customtkinter.CTkButton(root, text="Informe", command=gen_informe)
-    calculate.grid(row=16, column=0, pady=5, padx=50)
+    calculate.grid(row=15, column=1, pady=5, padx=10)
 
     root.mainloop()
